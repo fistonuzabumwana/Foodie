4e6eb602faf6fd4d42ec792d1ac7636163ffbf97
UZABUMWANA Fiston <youfistalline11@gmail.com>
2025-05-17 03:19:46 +0200

    persistence mongo added

--- src/main/java/com/example/Foodie/config/PersistenceConfig.java
+++ src/main/java/com/example/Foodie/config/PersistenceConfig.java
@@ -2,6 +2,7 @@ package com.example.Foodie.config; // Or your main application package
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 // No need to import EnableMongoRepositories if you don't have MongoRepository interfaces
 
 @Configuration
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 // you do not need @EnableMongoRepositories.
 // The GridFsTemplate used in FileStorageServiceImpl will still be auto-configured
 // by spring-boot-starter-data-mongodb.
+@EnableMongoRepositories(basePackages = "com.example.Foodie.mongorepo") // Example package
 public class PersistenceConfig {
     // You can leave this class empty if all you need is to specify the base packages.
 }
